0358f2bfb77b2f93cd48e47d380a5ccd11258f1d
jodiezhu <jodie.zhu@mail.utoronto.ca>
2017-10-06 14:52:41 -0400

    Update leetcode_315.py

--- leetcode_315.py
+++ leetcode_315.py
@@ -15,23 +15,49 @@ Return the array [2, 1, 1, 0].
 
 class Solution(object):
     def countSmaller(self, nums):
-        def sort(enum):
-            half = int(len(enum) / 2)
-            if half:
-                left, right = sort(enum[:half]), sort(enum[half:])
-                for i in range(len(enum))[::-1]:
-                    if not right or left and left[-1][1] > right[-1][1]:
-                        smaller[left[-1][0]] += len(right)
-                        enum[i] = left.pop()
-                    else:
-                        enum[i] = right.pop()
-            return enum
+        sorted=[]
+        ans=[0]*len(nums)
 
-        smaller = [0] * len(nums)
+        for i in range(len(nums)-1,-1,-1):
+            index=self.findIndex(sorted,nums[i])
+            ans[i]=index
+            sorted.insert(index,nums[i]) ##Using insert!!!!!!
+        return ans
 
-        sort(list(enumerate(nums))) #[(id1,val1),(id2,val2),....,(idn,valn)]
-        return smaller
+    def findIndex(self,sorted,target):
 
+        start=0
+        end=len(sorted)-1
+        #all special cases:
+        if len(sorted)==0: return 0
+        if sorted[end]<target: return end+1
+        if sorted[start]>=target: return 0
 
+        while start<end:
+            mid=int(start+(end-start)/2)
+            if sorted[mid]<target:
+                start=mid+1
+            else:
+                end=mid
+        return start
+
+'''
+#second way of findIndex
+def findIndex(self,sorted,target):
+
+        start=0
+        end=len(sorted)-1
+
+        if len(sorted)==0: return 0
+
+        while start<=end:
+            mid=int(start+(end-start)/2)
+            if sorted[mid]<target:
+                start=mid+1
+            else:
+                end=mid-1
+        return start
+
+'''
 s=Solution()
-print(s.countSmaller([2,4,2,1]))
+print(s.countSmaller([5,2,6,1]))
