12c784ea352f0b731e2db8fadad5dcddc42ed4b7
jodiezhu <jodie.zhu@mail.utoronto.ca>
2017-10-18 16:05:49 -0400

    Update leetcode_629_un.py

--- leetcode_629_un.py
+++ leetcode_629_un.py
@@ -1,24 +1,24 @@
 class MedianFinder(object):
     def kInversePairs(self, n, k):
         MOD = 10**9 + 7
-
-        dq = []    
-        for i in range(n): # create a list with nested lists
-            dq.append([])
-                
-        dq[2][0]=1
-        dq[2][1]=1
-        
         if k==0 or k==n*(n-1)/2: return 1
-        if k<0 or k>n*(n-1)/2: return 0
+        elif k<0 or k>n*(n-1)/2: return 0
+
+        else:
+            dq = [[0]*(k+1) for _ in range(n+1)]    #(n+1) by (k+1) Matrix
+                
+            dq[2][0]=1
+            dq[2][1]=1
         
-        for i in range(3,n+1):
-            if k==0: return 1
-            for j in range(1,i+1):
-                dq[i][j]=dp[i-1][j]+dp[i][j-1]
-                if j>=i: dq[i][j]-=dp[i-1][j-i]
-                dq[i][j]=(dq[i][j]+MOD)% MOD
-        return dq[n][k] 
+            for i in range(3,n+1):
+                dq[i][0]=1
+                for j in range(1,k+1):
+                    dq[i][j]=dq[i-1][j]+dq[i][j-1]
+                    if j>=i: dq[i][j]-=dq[i-1][j-i]
+                    dq[i][j]=(dq[i][j]+MOD)% MOD
+        
+            return dq[n][k]
+         
     
 s=MedianFinder()
-print(s.kInversePairs(3,1))   
+print(s.kInversePairs(7,2)) 
