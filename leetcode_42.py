caf8287fd3aa29eaec33530e6f7f25aafabe9e16
jodiezhu <jodie.zhu@mail.utoronto.ca>
2017-10-26 15:38:46 -0400

    Update leetcode_42.py

--- leetcode_42.py
+++ leetcode_42.py
@@ -1,1 +1,29 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left=0 
+        right=len(height)-1
 
+        max_left=max_right=res=0
+
+        while left<=right:
+            if height[left]<=height[right]:
+                if height[left]>=max_left:
+                    max_left=height[left]
+                else:
+                    res+=max_left-height[left]
+                left+=1
+
+            else:
+                if height[right]>=max_right:
+                    max_right=height[right]
+                else:
+                    res+=max_right-height[right]
+                right-=1           
+        return res
+
+s=Solution()
+print(s.trap([0,1,0,2,1,0,1,3]))
